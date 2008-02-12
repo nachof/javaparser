740708a5f1ea843cbc2c2ccfa939c75f0f2fc8d5
Nacho <nachofacello@gmail.com>
2008-02-12 13:28:02 -0200

    Los HashParser simulan ser stateless ahora. Funciona la recursión. Los Ewoks derrotan al Imperio!!!

--- HashParser.java
+++ HashParser.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+import java.util.EmptyStackException;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -6,6 +8,8 @@ public abstract class HashParser extends Parser
 {
     private Map retorno;
     private String finalString;
+
+    private Stack stateStack = new Stack();
 
     protected void discard(Parser p) {
         p.parse(finalString);
@@ -24,10 +28,51 @@ public abstract class HashParser extends Parser
     }
 
     public ParseResult doParse(String s) {
+        saveState();
         finalString = s;
         retorno = new HashMap();
         runParser();
-        return new ParseResult(retorno, finalString);
+        ParseResult resultado = new ParseResult(retorno, finalString);
+        restoreState();
+        return resultado;
+    }
+
+    private void saveState() {
+        stateStack.push(new HashParserState(retorno, finalString));
+    }
+
+    private void restoreState() {
+        try {
+            HashParserState state = (HashParserState) stateStack.pop();
+            retorno = state.getRetorno();
+            finalString = state.getFinalString();
+        } catch (EmptyStackException e) {
+            // Do nothing -- no state to restore (shouldn't happen, in fact
+        }
+    }
+
+    private class HashParserState {
+        private Map retorno;
+        private String finalString;
+
+        public HashParserState(Map retorno, String finalString) {
+            if (retorno == null)
+                this.retorno = new HashMap();
+            else
+                this.retorno = new HashMap(retorno);
+            if (finalString == null) 
+                this.finalString = "";
+            else
+                this.finalString = new String(finalString);
+        }
+
+        public Map getRetorno() {
+            return this.retorno;
+        }
+
+        public String getFinalString() {
+            return this.finalString;
+        }
     }
 
     public abstract void runParser();
